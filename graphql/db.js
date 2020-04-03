6d49d655cb025595eb34b1a1b35c4ec2853d564f
alstjs1207 <alstjs1207@naver.com>
2020-04-03 20:28:32 +0900

    Mutaion Test

--- graphql/db.js
+++ graphql/db.js
@@ -31,9 +31,19 @@ let movies = [
   export const deleteMovie = (id) => {
     const cleanedMovies = movies.filter(movie => movie.id !== id);
     if(movies.length > cleanedMovies.length){
-        movie = cleanedMovies;
+        movies = cleanedMovies;
         return true;
     } else {
         return false;
     }
-  }
+  };
+
+  export const addMovie = (name, score) => {
+      const newMovie = {
+        id: `${movies.length + 1}`,  
+        name,
+        score
+      };
+      movies.push(newMovie);
+      return newMovie;
+  };
