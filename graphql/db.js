052ef49b306c966274fe79a52a5aa8fe614c90ba
alstjs1207 <alstjs1207@naver.com>
2020-04-13 21:34:14 +0900

    REST API with GraphQL

--- graphql/db.js
+++ graphql/db.js
@@ -1,49 +1,16 @@
-let movies = [
-    {
-      id: 0,
-      name: "kingdom",
-      score: 1
-    },
-    {
-        id: 1,
-        name: "avengers",
-        score: 8
-    },
-    {
-        id: 2,
-        name: "ironman",
-        score: 99
-    },
-    {
-        id: 3,
-        name: "Nicolas",
-        score: 2
-    }
-  ];
+import fetch from "node-fetch";
+const API_URL = "https://yts.am/api/v2/list_movies.json?";
 
-  export const getMovies = () => movies;
-  
-  export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === id);
-    return filteredMovies[0];
-  };
+export const getMovies = (limit, rating) => {
+  let REQUEST_URL = API_URL;
+  if(limit > 0) {
+    REQUEST_URL += `limit=${limit}`;
+  }
+  if(rating > 0) {
+    REQUEST_URL += `&minimum_rating=${rating}`;
+  }
 
-  export const deleteMovie = (id) => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id);
-    if(movies.length > cleanedMovies.length){
-        movies = cleanedMovies;
-        return true;
-    } else {
-        return false;
-    }
-  };
+  return fetch(REQUEST_URL)
+  .then(res => res.json()).then(json => json.data.movies);
 
-  export const addMovie = (name, score) => {
-      const newMovie = {
-        id: `${movies.length + 1}`,  
-        name,
-        score
-      };
-      movies.push(newMovie);
-      return newMovie;
-  };
+};
